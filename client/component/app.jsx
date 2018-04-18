dbc868d6b3486ee856ecc19b74a6be79e69a09f5
Derek DeCoste <dsquaredslim@gmail.com>
2018-04-18 14:13:57 -0700

    completed simple version searching for location with no typing options

--- client/component/app.jsx
+++ client/component/app.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Search from './search.jsx';
 import Weather from './weather.jsx';
-import $ from 'jquery';
 import '../public/css/style.css';
+const axios = require('axios');
 
 class App extends React.Component {
   constructor() {
@@ -10,8 +9,6 @@ class App extends React.Component {
     this.state = {
       lat: 34,
       lon: -118,
-      latLock: 34,
-      lonLock: -118,
       weather: {
         latitude: '',
         longitude: '',
@@ -25,61 +22,69 @@ class App extends React.Component {
         dailyPrecipProbability: '',
       }
     }
-    this.onLatChange = this.onLatChange.bind(this);
-    this.onLonChange = this.onLonChange.bind(this);
-    this.onPush = this.onPush.bind(this);
-
+    this.search = this.search.bind(this);
+    this.geoSuccess = this.geoSuccess.bind(this);
   }
 
   componentDidMount() {
-    this.search(this.state.lon, this.state.lat);
+    //this.search(this.state.lon, this.state.lat);
+    if (navigator.geolocation) {
+      this.geoSuccess();
+    }
+    else {
+      console.log('Geolocation is not supported for this Browser/OS.');
+    }
+  }
+  
+  geoSuccess() {
+    var refThis = this;
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos.coords.latitude, pos.coords.longitude);
+      refThis.setState({
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      });
+      this.search(pos.coords.longitude, pos.coords.latitude);
+    });
+    //document.getElementById('startLon').innerHTML = startPos.coords.longitude;
   }
 
   search(lon, lat) {
+    var refThis = this;
     console.log('searching...lat then lon ', lat, lon)
-    $.post('/search', {lon: lon, lat: lat} , (dataPost) => { // make this restful later but for now display data on page
-      //$.get('/search', (dataGet) => {
-        var temp = JSON.parse(dataPost);
-        console.log('post data ', temp);
-      this.setState({ weather: temp});
-      //});
+    // $.post('/search', {lon: lon, lat: lat} , (dataPost) => { // make this restful later but for now display data on page
+    //   //$.get('/search', (dataGet) => {
+    //     var temp = JSON.parse(dataPost);
+    //     console.log('post data ', temp);
+    //   this.setState({ weather: temp});
+    //   //});
+    // });
+
+    axios.post('/search', {
+      lon: lon,
+      lat: lat
+    })
+    .then((dataPost) => {
+      console.log('post data ', dataPost);
+      refThis.setState({
+        weather: dataPost.data
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  }
-
-  onLatChange(e) {
-    this.setState({ lat: e });
-    console.log(this.state.lat, ' is the lat and lon is ', this.state.lon, ' and lonLock is ', this.state.lonLock, ' and latLock is ', this.state.latLock);
-    return false;
-  }
-
-  onPush() {
-    this.setState({ latLock: this.state.lat, lonLock: this.state.lon});
-    this.search(this.state.lon, this.state.lat);
-  }
-
-  onLonChange(e) {
-    this.setState({ lon: e });
-    return false;
-  }
-
-  initMap() {
-    // map = new google.maps.Map(document.getElementById('map'), {
-    //   center: {lat: -34.397, lng: 150.644},
-    //   zoom: 8
-    //})
   }
 
   render() {
     return (
       <div className ="container">
         <div className="mapAndSearch">
-          <Search lon={this.state.lon} lat={this.state.lat} onPush ={this.onPush} onLatChange={this.onLatChange} onLonChange={this.onLonChange}/>
           <br></br>
           <iframe id="map"
               width="600"
               height="450"
               frameborder="0" style={{border:0}}
-              src={`https://www.google.com/maps/embed/v1/view?key=AIzaSyD-wL3HWTzUKcYv8jUqusUYNRidG1B8OX0&center=${this.state.latLock},${this.state.lonLock}
+              src={`https://www.google.com/maps/embed/v1/view?key=AIzaSyD-wL3HWTzUKcYv8jUqusUYNRidG1B8OX0&center=${this.state.lat},${this.state.lon}
               &zoom=7`} allowfullscreen>
           </iframe>
         </div>
--- rest-server/index.js
+++ rest-server/index.js
@@ -4,22 +4,21 @@ const path = require('path');
 const PORT = 3000;
 const bodyParser = require('body-parser');
 const weather = require('./helpers/weather.js')
-const db = require('../db/index.js')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 app.post('/search', (req, res) => {
-  req.on('data', data => {
-    var arr = data.toString().split("=");
-    var lat = Number(arr.pop());
-    arr = arr[1].toString().split("&");
-    var lon = Number(arr.shift());
-    weather.getData(lat, lon, (err, resp, body) => {
-      if(err) {
-        console.log('error getting data', err)
-      } else {
-        var weather = JSON.parse(body);
-        if(weather.latitude) {
+  console.log(req.body)
+  var lat = req.body.lat, lon = req.body.lon;
+  weather.getData(lat, lon, (err, resp, body) => {
+    if(err) {
+      console.log('error getting data', err)
+      res.send(err);
+    } else {
+      var weather = JSON.parse(body);
+      if(weather.latitude) {
         var t = new Date(weather.currently.time*1000);
         weather = { // I want to pass this weather object back up to react to update the DOM w/
           latitude: weather.latitude,
@@ -33,20 +32,12 @@ app.post('/search', (req, res) => {
           dailyLow: weather.daily.data[0].temperatureLow,
           dailyPrecipProbability: weather.daily.data[0].precipProbability,
         };
-        db.save(weather, () => {
-          console.log('i think this was saved ', weather);
-          res.send(JSON.stringify(weather));
-        });
+        res.send(JSON.stringify(weather));
       } else {
           res.send('weather was undefined')
       }
-      }
-    });
+    }
   });
-});
-
-app.get('/search', (req, res) => {
-  res.end();
 });
 
 app.listen(PORT, () => {
